2219ec9d7fd587267ab574dd1e8c61fedf784318
Kon Toshihito <kon-toshihito@kayac.com>
2023-08-09 17:40:56 +0900

    オプション追加: output, version

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"os"
 	"text/template"
+
+	"github.com/spf13/pflag"
 )
+
+var Version string = "default version"
 
 func mustEnv(env string) (string, error) {
 	res := os.Getenv(env)
@@ -17,21 +20,31 @@ func mustEnv(env string) (string, error) {
 }
 
 func main() {
-	args := os.Args
+	var optVersion = pflag.BoolP("version", "v", false, "Print version")
+	var optOutput = pflag.StringP("out", "o", "", "Output file path")
+	pflag.Parse()
+	_ = optOutput
+
+	if *optVersion {
+		fmt.Fprintln(os.Stdout, Version)
+		os.Exit(0)
+	}
+
+	args := pflag.Args()
 	if len(args) == 0 {
 		printUsage()
 		os.Exit(1)
 	}
 
-	f, err := os.Open(args[1])
+	f, err := os.Open(args[0])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "os.Open: %s", err)
+		fmt.Fprintf(os.Stderr, "os.Open: %s\n", err)
 	}
 	defer f.Close()
 
 	body, err := io.ReadAll(f)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "io.ReadAll: %s", err)
+		fmt.Fprintf(os.Stderr, "io.ReadAll: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -42,17 +55,29 @@ func main() {
 
 	tmpl, err := template.New("template").Funcs(funcMap).Parse(string(body))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "parsing: %s", err)
+		fmt.Fprintf(os.Stderr, "parsing: %s\n", err)
 		os.Exit(1)
 	}
 
-	if err := tmpl.Execute(os.Stdout, nil); err != nil {
-		fmt.Fprintf(os.Stderr, "execution: %s", err)
+	var outFile *os.File
+	if *optOutput != "" {
+		outFile, err = os.Create(*optOutput)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "os.Create: %s\n", err)
+			os.Exit(1)
+		}
+		defer outFile.Close()
+	} else {
+		outFile = os.Stdout
+	}
+
+	if err := tmpl.Execute(outFile, nil); err != nil {
+		fmt.Fprintf(os.Stderr, "execution: %s\n", err)
 		os.Exit(1)
 	}
 }
 
 func printUsage() {
 	fmt.Fprintln(os.Stderr, `Usage: text-env template_file_path`)
-	flag.PrintDefaults()
+	pflag.PrintDefaults()
 }
